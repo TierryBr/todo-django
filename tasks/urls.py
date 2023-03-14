5d64672de9b25e62064763571ad768dd48b40ba6
TierryBr <tierrybarros15@gmail.com>
2023-03-14 09:05:27 -0300

    [main] - Created task

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from tasks.views import taskList, taskView, yourName
+from tasks.views import taskList, taskView, newTask, yourName
 
 urlpatterns = [
   path('', taskList, name='task-list'),
   path('task/<int:id>', taskView, name='task-view'),
+  path('newtask/', newTask, name='new-task'),
   path('yourname/<str:name>', yourName, name='your-name'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,14 +1,27 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Task
+from .forms import TaskForm
 
 def taskList(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.all().order_by('-created_at')
     return render(request, 'tasks/list.html', {'tasks': tasks})
 
 def taskView(request, id):
     task = get_object_or_404(Task, pk=id)
     return render(request, 'tasks/task.html', {'task': task})
+
+def newTask(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.done = 'doing'
+            task.save()
+            return redirect('/')
+    else:
+      form = TaskForm()
+      return render(request, 'tasks/addtask.html', {'form': form})
 
 def yourName(request, name):
     return render(request, 'tasks/yourname.html', {'name': name})
